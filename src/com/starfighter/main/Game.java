f80d7bfde36691ac28a208a6ac64419c95f99c13
Chaynor Hsiao <chaynsaw@gmail.com>
2021-04-01 06:57:22 -0700

    Adjust formatting in Game.java

--- src/com/starfighter/main/Game.java
+++ src/com/starfighter/main/Game.java
@@ -22,35 +22,36 @@ public class Game extends Canvas implements Runnable {
     public int ammo = 100;
 
     //constructor
-    public Game(){
-        new Window(1000,563,"StarFighter", this); //size
+    public Game() {
+        new Window(1000, 563, "StarFighter", this); //size
         start();
 
         handler = new Handler();
-        camera = new Camera(0,0);
+        camera = new Camera(0, 0);
         this.addKeyListener(new KeyInput(handler));
 
 
         BufferedImageLoader loader = new BufferedImageLoader();
         level = loader.loadImage("/StarShipWorld.png");
-        graphics = loader. loadImage("/textures.png");
+        graphics = loader.loadImage("/textures.png");
 
         gg = new ObjectGraphics(graphics);
 
-        space = gg.grabImage(2,1,64,64);
+        space = gg.grabImage(2, 1, 64, 64);
 
-        this.addMouseListener(new MouseInput(handler,camera, this, gg));
+        this.addMouseListener(new MouseInput(handler, camera, this, gg));
         loadLevel(level);
     }
 
 
     // Runnable
-    private void start(){
+    private void start() {
         isRunning = true;
         thread = new Thread(this); // we are calling this class run method
         thread.start();
     }
-    private void stop(){
+
+    private void stop() {
         isRunning = false;
         try {
             thread.join();
@@ -60,7 +61,7 @@ public class Game extends Canvas implements Runnable {
     }
 
     // CREDITS:Markus Alexej Persson AKA Notch developer of Minecraft
-    public void run(){
+    public void run() {
         this.requestFocus();
         long lastTime = System.nanoTime();
         double amountOfTicks = 60.0;
@@ -72,14 +73,14 @@ public class Game extends Canvas implements Runnable {
             long now = System.nanoTime();
             delta += (now - lastTime) / ns;
             lastTime = now;
-            while (delta >=1){
+            while (delta >= 1) {
                 tick();
                 delta--;
             }
             render();
             frames++;
 
-            if(System.currentTimeMillis() - timer > 1000){
+            if (System.currentTimeMillis() - timer > 1000) {
                 timer += 1000;
                 frames = 0;
             }
@@ -87,10 +88,10 @@ public class Game extends Canvas implements Runnable {
         stop();
     }
 
-    public void tick(){
+    public void tick() {
 
-        for(int i = 0; i < handler.object.size(); i++){
-            if(handler.object.get(i).getId() == ID.PLAYER){
+        for (int i = 0; i < handler.object.size(); i++) {
+            if (handler.object.get(i).getId() == ID.PLAYER) {
                 camera.tick(handler.object.get(i));
             }
         }
@@ -98,9 +99,9 @@ public class Game extends Canvas implements Runnable {
         handler.tick();
     }
 
-    public void render(){
+    public void render() {
         BufferStrategy bs = this.getBufferStrategy();
-        if(bs == null){
+        if (bs == null) {
             this.createBufferStrategy(3);
             return;
         }
@@ -109,13 +110,13 @@ public class Game extends Canvas implements Runnable {
         Graphics2D g2d = (Graphics2D) g;
         ////////////////////////////////
         g.setColor(Color.gray);
-        g.fillRect(0,0,getWidth(),getHeight());
+        g.fillRect(0, 0, getWidth(), getHeight());
 
         g2d.translate(-camera.getX(), -camera.getY());
 
-        for(int xx = 0; xx < 30*72; xx+=64){
-            for(int yy = 0; yy < 30*72; yy+=64){
-                g.drawImage(space,xx,yy,null);
+        for (int xx = 0; xx < 30 * 72; xx += 64) {
+            for (int yy = 0; yy < 30 * 72; yy += 64) {
+                g.drawImage(space, xx, yy, null);
             }
         }
 
@@ -132,28 +133,28 @@ public class Game extends Canvas implements Runnable {
     }
 
     // loading the level
-    private void loadLevel(BufferedImage image){
+    private void loadLevel(BufferedImage image) {
         int w = image.getWidth();
         int h = image.getHeight();
 
-        for (int xx = 0; xx < w; xx++){
-            for(int yy = 0; yy < h; yy++){
+        for (int xx = 0; xx < w; xx++) {
+            for (int yy = 0; yy < h; yy++) {
                 int pixel = image.getRGB(xx, yy);
-                int red = (pixel >> 16 ) & 0xff;
-                int green = (pixel >> 8 ) & 0xff;
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
                 int blue = (pixel) & 0xff;
 
-                if(red == 255)
-                    handler.addObject(new Block(xx*32, yy*32, ID.BLOCK, gg));
+                if (red == 255)
+                    handler.addObject(new Block(xx * 32, yy * 32, ID.BLOCK, gg));
 
-                if(blue == 255 && green == 0)
-                    handler.addObject(new StarShip(xx*32, yy*32, ID.PLAYER, handler, this, gg));
+                if (blue == 255 && green == 0)
+                    handler.addObject(new StarShip(xx * 32, yy * 32, ID.PLAYER, handler, this, gg));
 
-                if(green == 255 && blue == 0)
-                    handler.addObject(new Enemy(xx*32, yy*32, ID.ENEMY, gg,  handler));
+                if (green == 255 && blue == 0)
+                    handler.addObject(new Enemy(xx * 32, yy * 32, ID.ENEMY, gg, handler));
 
-                if(green == 255 && blue == 255)
-                    handler.addObject(new Crate(xx*32, yy*32, ID.CRATE, gg));
+                if (green == 255 && blue == 255)
+                    handler.addObject(new Crate(xx * 32, yy * 32, ID.CRATE, gg));
             }
         }
     }
